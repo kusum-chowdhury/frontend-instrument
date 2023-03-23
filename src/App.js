42739dbf45e548ebdcff1cbb332cbf9f16d82574
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-03-23 18:57:09 +0530

    added headline to about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
 
 function App() {
   return (
     <div className="App">
     <Navbar />
     <HeroSection />
+    <AboutSection />
     </div>
   );
 }
---
+++ src/components/AboutSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/AboutSection.css'
+
+function AboutSection() {
+  return (
+    < div className='about'>
+        <p className='headline'>We build connected brand systems from vision through execution</p>
+    </div>
+  )
+}
+
+export default AboutSection
