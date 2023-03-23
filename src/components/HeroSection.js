3b36a6929ecd46473829b771fc80a0cf67e98e97
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-03-23 18:16:03 +0530

    added video in hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from "react";
+import "../styles/HeroSection.css";
+
+function HeroSection() {
+
+
+
+  return (
+    <div className="hero-container">
+     <iframe src="https://player.vimeo.com/video/718558198?h=4bb86245d6" width="1750" height="1000" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+    </div>
+  );
+}
+export default HeroSection;
