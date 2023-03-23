ebe7112153eae9b9bd01c9978fe4eccc7928c7f7
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-03-23 22:28:54 +0530

    done with about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,14 +7,23 @@ function AboutSection() {
     < div className='about'>
         <p className='headline'>We build connected brand systems from vision through execution</p>
         <div className='img-text'>
-            <img src='https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'/>
+            <img src='https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' alt="about" />
             <div className='text'>
              <div>As a full-service partner to the world’s most ambitious companies, we create transformational change through best-in-class digital products and communications.</div>
              <button>What we do <HiArrowLongRight/></button>
             </div>
-        </div>
+            </div>
+        <div className='img-text'>
+            
+            <div className='text'>
+             <div>Instrument Named Among World's Most Innovative Companies in Design</div>
+             <button>What we do <HiArrowLongRight/></button>
+            </div>
+            <img src='https://images.unsplash.com/photo-1506097425191-7ad538b29cef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGlubm92YXRpdmUlMjBkZXNpZ258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt="about" />
+            </div>
+        <div/>
     </div>
   )
 }
 
-export default AboutSection
+export default AboutSection;
