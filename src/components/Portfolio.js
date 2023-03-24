49945a4c7600ac2d88777405f9d869a53579afdd
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-03-24 23:13:44 +0530

    created component for porfolio section

---
+++ src/components/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Portfolio.css'
+
+function Portfolio() {
+  return (
+    <div>
+        <p>Explore some of our most recent work</p>
+        <button>Explore our work</button>
+    </div>
+  )
+}
+
+export default Portfolio
