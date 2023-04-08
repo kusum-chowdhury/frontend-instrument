7d313f3cc6d9c33efe6663d3b4ed0a24ea7e1160
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-04-09 02:33:53 +0530

    created portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,6 +7,22 @@ function Portfolio() {
     <div className='porfolio'>
         <p>Explore some of our most recent work</p>
         <button>Our work <HiArrowLongRight /> </button>
+        <div className='portfolio-img-text'>
+          <div className='box'>
+          <img src='https://images.unsplash.com/photo-1549924231-f129b911e442?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' />
+          <div className='subtext-portfolio'>
+            <p className='co-name'>Eames Institute</p>
+            <div>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+          </div>
+          </div>
+          <div className='box'>
+          <img src='https://images.unsplash.com/photo-1455849318743-b2233052fcff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80' />
+          <div className='subtext-portfolio'>
+            <p className='co-name'>Eames Institute</p>
+            <div>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+          </div>
+          </div>
+        </div>
     </div>
   )
 }
